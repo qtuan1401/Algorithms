506d6c517bcfe81ea4ed04e726a781be19cc6596
Quoc Tuan Nguyen <qtuan14012001@gmail.com>
2024-06-29 12:04:45 +1000

    Add 1979c.cpp

--- codeforces/1978/c.cpp
+++ codeforces/1978/c.cpp
@@ -12,27 +12,6 @@ typedef long double ld;
 typedef long long ll;
 typedef pair < int, int > pii;
 
-bool calc(int n, long long val) {
-    long long sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += abs(i - (n - i - 1));
-    }
-    if (sum >= val) return true;
-    return false;
-}
-
-int bs(int l, int r, long long val) {
-    int pos = -1;
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (calc(mid, val)) {
-            pos = mid;
-            r = mid - 1;
-        } else l = mid + 1;
-    }
-    return pos;
-}
-
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("../test.in", "r", stdin);
@@ -44,38 +23,29 @@ int main() {
         int n;
         long long k;
         cin >> n >> k;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int s1 = abs(i - (n - i - 1)) + abs(j - (n - j - 1));
-                int s2 = abs(i - (n - j - 1)) + abs(j - (n - i - 1));
-                if (s1 - s2 == 4) cout << i << " " << j << endl;
+        if (k & 1) {
+            cout << "No\n";
+            continue;
+        }
+        vector < int > a(n, 0);
+        for (int i = 0; i < n; i++) a[i] = i;
+        for (int i = 0, j = n - 1; i < n; i++) {
+            if (k >= (j - i) * 2) {
+                k -= (j - i) * 2;
+                swap(a[i], a[j]);
+                j--;
+                if (k <= 0) break;
             }
         }
-        // if (k & 1) {
-        //     cout << "No\n";
-        //     continue;
-        // }
-        // int m = bs(1, n, k);
-        // if (m == -1) cout << "No\n";
-        // else {
-        //     cout << "Yes\n";
-        //     vector < int > ans(n);
-        //     long long sum = 0;
-        //     for (int i = 0; i < m; i++) {
-        //         ans[i] = m - i - 1;
-        //         sum += abs(ans[i] - i);
-        //     }
-        //     cout << m << " " << sum << " " << k << endl;
-        //     swap(ans[m / 2 - (sum - k) / 2], ans[m / 2]);
-        //     for (int i = m; i < n; i++) ans[i] = i;
-        //     for (int i = 0; i < n; i++) cout << ans[i] + 1 << " ";
-        //     cout << '\n';
-        //     sum = 0;
-        //     for (int i = 0; i < n; i++) {
-        //         sum += abs(ans[i] - i);
-        //     }
-        //     cerr << sum << endl;
-        // }
+
+        if (k == 0) {
+            cout << "Yes\n";
+            for (int i = 0; i < n; i++) cout << a[i] + 1 << " ";
+            cout << '\n';
+        } else {
+            cout << "No\n";
+            continue;
+        }
     }
     return 0;
 }
