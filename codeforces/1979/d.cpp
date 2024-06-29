dfea11912956d30fe7850fc9db34720af1cd87af
Quoc Tuan Nguyen <qtuan14012001@gmail.com>
2024-06-29 13:01:05 +1000

    Add 1979d.cpp

--- codeforces/1979/d.cpp
+++ codeforces/1979/d.cpp
@@ -19,6 +19,16 @@ int getSum(int l, int r) {
     return sum[r] - sum[l - 1];
 }
 
+bool check(int i, int k, int n, vector < int > & a, string & s) {
+    if (a[i + 1] == 1) return true;
+    if (s[i] != s[i + 1]) return a[i + 1] != 0;
+    int len = sum[i + k];
+    len -= sum[i];
+    if (len != k) return false;
+    if (i + k + 1 >= n) return true;
+    return a[i + k + 1];
+}
+
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("../test.in", "r", stdin);
@@ -37,42 +47,50 @@ int main() {
             if (s[i] == s[i - 1]) sum[i] = sum[i - 1] + 1;
             else sum[i] = 1;
         }
-
         vector < int > fr(n, -1);
         for (int i = n - 1; i >= 0; i--) {
-            int l = i - k + 1;
-            if (fr[l] == -1) fr[l] = sum[i] == k;
-            fr[l] = fr[l] && (sum[i] == k);
-            if (l - k >= 0) fr[l - k] = fr[l];
+            if (i + k - 1 < n) {
+                fr[i] = sum[i + k - 1] == k;
+                if (i + k < n && fr[i + k] != -1) fr[i] &= fr[i + k];
+            }
         }
-        for (int i = 0; i < n; i++) cout << fr[i] << " ";
-        cout << endl;
+        // for (int i = 0; i < n; i++) cout << fr[i] << " ";
+        // cout << '\n';
+        if (fr[0] == 1) {
+            cout << n << " ";
+            cout << '\n';
+            continue;
+        }
         bool ok = true;
         int p = -1, l = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - k; i++) {
+            // cout << i << endl;
             if (sum[i] > k) break;
-            if (i % k == 0) l = i;
-            if (i + k < n) {
-                if (fr[i + 1] == 0) continue;
-            }
-            if (i % k == 0 && i > 0) {
-                ok = (getSum(i - k, i - 1) == k) && (s[i] != s[i - 1]);
-                if (ok == false) {
+            if (i % k == 0) {
+                l = i;
+                if (i > 0 && (s[i - 1] == s[i] || sum[i - 1] != k)) {
                     break;
                 }
             }
-            if ((i + 1) % k == 0) {
-                if (getSum(l, i) == k && s[n - 1] != s[l]) {
-                    p = i;
+
+            if (check(i, k, n, fr, s) == false) continue;
+            // cout << i % k << " " << k - 1 << endl;
+            if (i % k == k - 1) {
+                // cout << sum[i] << " " << endl;
+                if (sum[i] == k && s[i] != s[n - 1]) {
+                    p = i + 1;
+                    break;
+                }
+            } else {
+                if (s[i] != s[n - 1]) continue;
+                int sufLen = k - ((i + 1) % k);
+                if (sum[n - 1] != sufLen) continue;
+                if (sum[i] + sufLen == k) {
+                    p = i +1;
                     break;
                 }
             }
-            if (s[i] != s[l]) break;
-            cout << l << " " << i << " " << n - (k - i % k - 1) << endl;
-            if (sum[i] + getSum(n - (k - i % k - 1), n - 1) == k && s[l] == s[n - 1]) {
-                p = i;
-                break;
-            }
+
         }
         cout << p << '\n';
     }
